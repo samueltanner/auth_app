f0ce6259f99df090825787c338127feda7bbe9ff
samueltanner <samuel.m.s.tanner@gmail.com>
2021-03-09 19:14:31 -0800

    adds todays homework

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    if current_user
+      @user = User.find(current_user.id)
+      render "show.json.jb"
+    else
+      render json: { message: "You must log in first" }
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    post "/users" => "users#create"
+    get "/users/:id" => "users#show"
+    post "/sessions" => "sessions#create"
+  end
+end
